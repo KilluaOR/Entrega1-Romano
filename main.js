83c6dab352084c70635efba9ea08da3ea28d78ca
Orne <killu@gmail.com>
2025-07-21 16:23:27 -0300

    modificaciones en la estructura

--- main.js
+++ main.js
@@ -1,37 +1,64 @@
-let compra = () => {
-  const accionCompra = alert(
-    "Ve a la pestaña Buscar y busca tu libro de preferencia"
-  );
-};
-let venta = () => {
-  const accionVenta = alert(
-    "Ve a la pestaña Vender y publica el libro que desees vender"
-  );
-};
-let operacion = (op) => {
-  console.log("operacion", op);
-  switch (op) {
-    case 1:
-      compra();
-      break;
-    case 2:
-      venta();
-      break;
-    default:
-      principal();
-  }
-};
+const libreria = [
+  {
+    nombre: "The Enchanted Forest",
+    descripcion: "A tale of magic and adventure",
+    precio: "$20.000",
+  },
+  {
+    nombre: "Space Odyssey",
+    descripcion: "Journey through the cosmos",
+    precio: "$15.000",
+  },
+  {
+    nombre: "The Secret Code",
+    descripcion: "Unravel the mystery",
+    precio: "$30.000",
+  },
+];
 
-function llamarAction() {
-  let pregunta = prompt(`Qué quiere hacer:
-1_Comprar libros 
-2_Vender libros`);
+// Acá la idea es que cuando buscas un libro en el buscador, la página vaya mostrando solo las coincidencias//
+// function fn(libreria) {
+// 		const buscarLibro = libreria.find(
+// 			libro => libro.nombre ===
+// 		)
+// 		return ``
+// 	}
 
-  operacion(Number(pregunta));
-}
+// 	console.log(fn(libreria))
+// let compra = () => {
+//   const accionCompra = alert(
+//     "Ve a la pestaña Buscar y busca tu libro de preferencia"
+//   );
+// };
+// let venta = () => {
+//   const accionVenta = alert(
+//     "Ve a la pestaña Vender y publica el libro que desees vender"
+//   );
+// };
+// let operacion = (op) => {
+//   console.log("operacion", op);
+//   switch (op) {
+//     case 1:
+//       compra();
+//       break;
+//     case 2:
+//       venta();
+//       break;
+//     default:
+//       principal();
+//   }
+// };
 
-function principal() {
-  llamarAction();
-  console.log("Arrancamos");
-}
-principal();
+// function llamarAction() {
+//   let pregunta = prompt(`Qué quiere hacer:
+// 1_Comprar libros
+// 2_Vender libros`);
+
+//   operacion(Number(pregunta));
+// }
+
+// function principal() {
+//   llamarAction();
+//   console.log("Arrancamos");
+// }
+// principal();
