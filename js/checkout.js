53bc2fb1e0f5e079e0a814729f203975e435ea25
Orne <killu@gmail.com>
2025-07-28 15:29:08 -0300

    modificando checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -1,2 +1,7 @@
-const params = new URLSearchParams(window.location.search);
-const bookId = params.get("id");
+// const params = new URLSearchParams(window.location.search);
+// const bookId = params.get("id");
+
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+
+button.addEventListener("click", handleClick);
