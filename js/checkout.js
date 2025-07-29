cd28064673ef8c9348756262326646c5fe323400
Orne <killu@gmail.com>
2025-07-29 15:53:43 -0300

    cambios en checkout

--- js/checkout.js
+++ js/checkout.js
@@ -1,7 +1,12 @@
 // const params = new URLSearchParams(window.location.search);
 // const bookId = params.get("id");
-
-const button = document.querySelector("button");
+const section = document.querySelector("section");
+const button = document.querySelector("button.checkout");
 const input = document.querySelector("input");
 
 button.addEventListener("click", handleClick);
+
+function handleClick() {
+  section.innerHTML = `
+    <h2 class="cartel-final">Gracias por tu compra!</h2>`;
+}
